92cf0913318c0e115d31ef35c8921ec78843625a
Justine C <83780776+JordyKonpyuta@users.noreply.github.com>
2025-02-20 16:48:39 +0100

    Setup all animations and prefabs

---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInteraction : MonoBehaviour
+{
+
+    public bool hasGearBoyEquipped = false;
+    public bool isTalking = false;
+    public bool isInteracting = false;
+
+    public GameObject interactionCanvas;
+    public Text interactionText;
+
+    public Animator animator;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private static readonly int Speed = Animator.StringToHash("Speed");
+    public float moveSpeed;
+
+    public Rigidbody2D rigidBody;
+    private Vector3 _velocity = Vector3.zero;
+
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+    
+    void FixedUpdate()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        
+        MovePlayer(horizontalMovement);
+        Flip(rigidBody.linearVelocity.x);
+        
+        float characterVelocity = Mathf.Abs(rigidBody.linearVelocity.x);
+        animator.SetFloat(Speed, characterVelocity);
+    }
+
+    void MovePlayer(float movement)
+    {
+        Vector3 targetVelocity = new Vector2(movement, rigidBody.linearVelocity.y);
+        rigidBody.linearVelocity = Vector3.SmoothDamp(rigidBody.linearVelocity, targetVelocity, ref _velocity, .05f);
+    }
+
+    void Flip(float velocity)
+    {
+        if (velocity > 0.1f) spriteRenderer.flipX = true;
+        else if (velocity < -0.1f) spriteRenderer.flipX = false;
+    }
+}
